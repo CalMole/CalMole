42aad18720df9bf6c85ba29acb3bdc49f04c8c72
Brian Grosso <bgro63@gmail.com>
2021-07-31 14:35:05 -0400

    adds new pkgs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <a href="/auth/google">Log In with Google</a>
       </header>
     </div>
   );
